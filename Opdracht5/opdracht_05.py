e308433fb50eca884f57a4ce031b1be28aea6d3e
Mohamed Jamai <2150953@talnet.nl>
2021-10-26 14:05:45 +0200

    onder de knie

--- Opdracht5/opdracht_05.py
+++ Opdracht5/opdracht_05.py
@@ -1,3 +1,15 @@
 #maak hier een functie voor een top 3 van bijvoorbeeld je favoriete games, eten of iets anders
 #elke keer dat je de functie aanroept voeg je een nieuwe game toe als parameter
 #weet je niet meer hoe? Check het solution filmpje op: https://www.linkedin.com/learning/programming-foundations-fundamentals-3/solution-favorite-cities
+
+
+print ("De top 3 games van Mohamed")
+
+
+def Mohamed (message):
+    print(message)
+
+
+Mohamed("Warzone")
+Mohamed("Minecraft")
+Mohamed("Apex Legends")
